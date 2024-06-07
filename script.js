0d46d9faf62de4a56c4bc335bfdb29f7c1253142
peranidzeanano <peranidzeanano04@gmail.com>
2024-06-07 20:38:05 +0400

    add some javascript and made it more responsive

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("hellow world");
+
+const yearEL = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEL.textContent = currentYear;
+
+const header = document.querySelector(".navigation");
+header.addEventListener("click", function () {
+  header.classList.toggle("open");
+});
